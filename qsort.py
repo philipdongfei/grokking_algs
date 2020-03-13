ab4645e927c7144e4e363ed69d576aa3bc367ddf
PhlipTung <philip.dongfei@gmail.com>
2020-03-13 17:36:48 +0800

    modify qsort.py add print_items, print_items2

--- qsort.py
+++ qsort.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def quicksort(array):
     if len(array) < 2:
         return array # Base case: arrays with 0 or 1 element are already "sorted"
@@ -10,5 +12,17 @@ def quicksort(array):
 
         return quicksort(less) + [pivot] + quicksort(greater)
 
+def print_items(List):
+    for item in List:
+        print(item)
+
+def print_items2(List):
+    for item in List:
+        sleep(1)
+        print(item)
+
 print(quicksort([10,5,2,3]))
+t = [2,4,6,8,10]
+print_items(t)
+print_items2(t)
 
